e1ccc46f3addc2c63a4d7a925380583e7d8a4116
pjdufour <pjdufour.dev@gmail.com>
2017-04-08 14:34:54 -0400

    initial work

---
+++ components/GeoDashComponentMap.ts
@@ -0,0 +1,26 @@
+declare var extract: any;
+declare var geodash: any;
+
+/* Components */
+import { Component, OnInit, EventEmitter, ElementRef } from '@angular/core';
+
+/* Services */
+import { GeoDashServiceBus }  from 'GeoDashServiceBus';
+
+@Component({
+  selector: 'geodash-map',
+  template: geodash.api.getTemplate('geodashMap.tpl.html')
+})
+export class GeoDashComponentMap implements OnInit {
+  name = 'GeoDashComponentMap';
+
+  constructor(private element: ElementRef, private bus: GeoDashServiceBus) {
+
+  }
+
+  ngOnInit(): void {
+    geodash.var.components[this.name] = this; // register externally
+
+  }
+
+}
